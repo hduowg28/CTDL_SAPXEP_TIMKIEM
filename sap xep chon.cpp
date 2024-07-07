b8d77d9b116d0eaad813c7b5d8dc6bf1f4764633
hduowg28 <hoangduongg2222@gmail.com>
2024-07-07 15:24:48 +0700

    them bai sap xep chon

---
+++ sap xep chon.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define pb(x) push_back(x)
+#define vi vector<int>
+#define all(x) x.begin(), x.end()
+using namespace std;
+void io();
+
+void solve(){
+    int n, k=1;
+    cin>>n;
+    vi v(n);
+    for(auto &x:v) cin>>x;
+    for(int i=0;i<n;i++){
+        int cnt=0,m=i;
+        for(int j=i+1;j<n;j++){
+            if(v[m]>v[j]) {m=j;cnt++;}
+        }
+        if(cnt){
+            swap(v[i], v[m]);
+            cout<<"Buoc "<<k++<<": ";
+            for(auto x:v) cout<<x<<" ";
+            cout<<"\n";
+        }
+    }
+}
+int main()
+{
+    io();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+        // cout<<cnt;
+    return 0;
+}
+void io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif  
+}
